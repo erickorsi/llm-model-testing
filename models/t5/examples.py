f1c6284277baed4cbf73c0e10e3b8866660c97ad
erickorsi <erickorsig@gmail.com>
2025-03-21 09:43:37 -0300

    feat: t5

---
+++ models/t5/examples.py
@@ -0,0 +1,85 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small")
+model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
+
+## TRAINING
+#--------1
+input_ids = tokenizer("The <extra_id_0> walks in <extra_id_1> park", return_tensors="pt").input_ids
+labels = tokenizer("<extra_id_0> cute dog <extra_id_1> the <extra_id_2>", return_tensors="pt").input_ids
+
+# the forward function automatically creates the correct decoder_input_ids
+loss = model(input_ids=input_ids, labels=labels).loss
+loss.item()
+
+#--------2
+input_ids = tokenizer("translate English to German: The house is wonderful.", return_tensors="pt").input_ids
+labels = tokenizer("Das Haus ist wunderbar.", return_tensors="pt").input_ids
+
+# the forward function automatically creates the correct decoder_input_ids
+loss = model(input_ids=input_ids, labels=labels).loss
+loss.item()
+
+#--------3 (batch)
+# the following 2 hyperparameters are task-specific
+max_source_length = 512
+max_target_length = 128
+
+# Suppose we have the following 2 training examples:
+input_sequence_1 = "Welcome to NYC"
+output_sequence_1 = "Bienvenue à NYC"
+
+input_sequence_2 = "HuggingFace is a company"
+output_sequence_2 = "HuggingFace est une entreprise"
+
+# encode the inputs
+task_prefix = "translate English to French: "
+input_sequences = [input_sequence_1, input_sequence_2]
+
+encoding = tokenizer(
+    [task_prefix + sequence for sequence in input_sequences],
+    padding="longest",
+    max_length=max_source_length,
+    truncation=True,
+    return_tensors="pt",
+)
+
+input_ids, attention_mask = encoding.input_ids, encoding.attention_mask
+
+# encode the targets
+target_encoding = tokenizer(
+    [output_sequence_1, output_sequence_2],
+    padding="longest",
+    max_length=max_target_length,
+    truncation=True,
+    return_tensors="pt",
+)
+labels = target_encoding.input_ids
+
+# replace padding token id's of the labels by -100 so it's ignored by the loss
+labels[labels == tokenizer.pad_token_id] = -100
+
+# forward pass
+loss = model(input_ids=input_ids, attention_mask=attention_mask, labels=labels).loss
+loss.item()
+
+## INFERENCE
+#-------1
+input_ids = tokenizer("translate English to German: The house is big.", return_tensors="pt").input_ids
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+
+#-------2 (batch)
+task_prefix = "translate English to German: "
+# use different length sentences to test batching
+sentences = ["The house is wonderful.", "I like to work in NYC."]
+
+inputs = tokenizer([task_prefix + sentence for sentence in sentences], return_tensors="pt", padding=True)
+
+output_sequences = model.generate(
+    input_ids=inputs["input_ids"],
+    attention_mask=inputs["attention_mask"],
+    do_sample=False,  # disable sampling to test if batching affects output
+)
+
+print(tokenizer.batch_decode(output_sequences, skip_special_tokens=True))
